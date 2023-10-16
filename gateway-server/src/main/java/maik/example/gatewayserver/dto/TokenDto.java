e973905d6af248a0136977f590c05607ad58e7f7
maiksurco <rickisurco@upeu.edu.pe>
2023-10-16 09:16:13 -0500

    auth2

---
+++ gateway-server/src/main/java/maik/example/gatewayserver/dto/TokenDto.java
@@ -0,0 +1,11 @@
+package maik.example.gatewayserver.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TokenDto {
+    private String token;
+}
