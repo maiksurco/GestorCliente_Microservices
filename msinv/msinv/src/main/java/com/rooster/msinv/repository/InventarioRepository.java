9f6b3a100035fd3505f94086f82d0a1f60e5b100
unknown <maiksurco>
2023-11-20 04:31:56 -0500

    complet

--- msinv/msinv/src/main/java/com/rooster/msinv/repository/InventarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rooster.msinv.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.rooster.msinv.entity.Inventario;
-
-public interface InventarioRepository extends JpaRepository<Inventario,Integer>{
-
-}
